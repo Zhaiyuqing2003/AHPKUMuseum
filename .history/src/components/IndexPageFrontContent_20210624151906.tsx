f406f55e4c174fa16221709952910e7ba40aa96e
DaltonWritingCenter <tanchuping@i.pkuschool.edu.cn>
2021-06-25 18:53:27 +0800

    timeline

---
+++ .history/src/components/IndexPageFrontContent_20210624151906.tsx
@@ -0,0 +1,118 @@
+import * as React from "react";
+
+import { useEffect, useState } from "react";
+
+import {
+    Card,
+    CardMedia,
+    CardContent,
+    Toolbar,
+    Box,
+    Typography,
+    Button,
+    makeStyles
+} from "@material-ui/core"
+
+import {
+    useTheme,
+    useMediaQuery
+} from "@material-ui/core"
+
+import BreakPointsType from "../utils/BreakPointsType"
+import useDeviceWidthQuery from "../utils/useDeviceWidthQuery"
+import SeasonType from "../utils/SeasonType"
+import useSeason from "../utils/useSeason"
+
+
+//@ts-ignore
+import springDesktop from "../images/index/SpringDesktop.jpg"
+//@ts-ignore
+import springLaptop from "../images/index/SpringLaptop.jpg"
+//@ts-ignore
+import springMobile from "../images/index/SpringMobile.jpg"
+
+//@ts-ignore
+import summerDesktop from "../images/index/SummerDesktop.jpg"
+//@ts-ignore
+import summerLaptop from "../images/index/SummerLaptop.jpg"
+//@ts-ignore
+import summerMobile from "../images/index/SummerMobile.jpg"
+
+//@ts-ignore
+import autumnDesktop from "../images/index/AutumnDesktop.jpg"
+//@ts-ignore
+import autumnLaptop from "../images/index/AutumnLaptop.jpg"
+//@ts-ignore
+import autumnMobile from "../images/index/AutumnMobile.jpg"
+
+//@ts-ignore
+import winterDesktop from "../images/index/WinterDesktop.jpg"
+//@ts-ignore
+import winterLaptop from "../images/index/WinterLaptop.jpg"
+//@ts-ignore
+import winterMobile from "../images/index/WinterMobile.jpg"
+
+const useIndexPageFrontContentStyle = makeStyles((theme) => ({
+    cardMedia : {
+        height : "110vh"
+    }
+}))
+
+
+function useImageDir(deviceMediaQuery : CustomBreakPointsType, seasonQuery: SeasonType){
+    switch (deviceMediaQuery){
+        case BreakPointsType.mobileS:
+        case BreakPointsType.mobileM:
+        case BreakPointsType.mobileL:
+            switch (seasonQuery){
+                case SeasonType.Spring:
+                    return springMobile
+                case SeasonType.Summer:
+                    return summerMobile
+                case SeasonType.Autumn:
+                    return autumnMobile
+                case SeasonType.Winter:
+                    return winterMobile
+            }
+        case BreakPointsType.tablet:
+            switch (seasonQuery){
+                case SeasonType.Spring:
+                    return springLaptop
+                case SeasonType.Summer:
+                    return summerLaptop
+                case SeasonType.Autumn:
+                    return autumnLaptop
+                case SeasonType.Winter:
+                    return winterLaptop
+            }
+        default:
+            switch (seasonQuery){
+                case SeasonType.Spring:
+                    return springDesktop
+                case SeasonType.Summer:
+                    return summerDesktop
+                case SeasonType.Autumn:
+                    return autumnDesktop
+                case SeasonType.Winter:
+                    return winterDesktop
+            }
+    }
+}
+
+export default function(){
+    const theme = useTheme()
+    const classes = useIndexPageFrontContentStyle()
+
+
+    const deviceWidthQuery = useDeviceWidthQuery(theme)
+    const season = useSeason();
+
+    const imageDir = useImageDir(deviceWidthQuery, SeasonType.Spring)
+
+    return (<Card>
+        <CardMedia
+            image = { imageDir }
+            className = { classes.cardMedia }>
+        </CardMedia>
+    </Card>)
+}
---
+++ .history/src/index_20210624214322.tsx
@@ -0,0 +1,103 @@
+import * as React from "react"
+import {
+    useState,
+    useEffect,
+    useMemo,
+    Suspense
+} from "react"
+import useOnceEffect from "./utils/useOnceEffect"
+import ReactDOM from "react-dom"
+
+import {
+    CssBaseline,
+    useMediaQuery,
+    Container
+} from "@material-ui/core"
+
+import { makeStyles } from "@material-ui/core/styles"
+import { ThemeProvider } from "@material-ui/core/styles"
+
+import StyledEngineProvider from "@material-ui/core/StyledEngineProvider"
+
+import './utils/I18n'
+import { useTranslation } from "react-i18next"
+
+import ModeType from "./utils/ModeType"
+import LanguageType from "./utils/LanguageType"
+
+import createAppTheme from "./utils/AppTheme"
+import changeWebIconTheme from "./utils/WebIconTheme"
+
+import "./utils/FontDependencies";
+
+import AppTopBar from "./components/AppTopBar"
+import IndexPageFrontContent from "./components/IndexPageFrontContent"
+import AppContainer from "./components/AppContainer"
+import CustomizedTimeline from './components/IndexTimeline'
+const useAppStyle = makeStyles((theme) => ({
+
+}))
+
+function main(): void{
+    const container = document.querySelector("#container")
+    ReactDOM.render(<Suspense fallback = " "><App /></Suspense>, container)
+}
+
+function App(){
+    const [modeType, setModeType] = useState(ModeType.LIGHT)
+    const [languageType, setLanguageType] = useState(LanguageType.zh_CN)
+    const isUserModeTypeDark = useMediaQuery("(prefers-color-scheme: dark)");
+
+    const classes = useAppStyle()
+
+    const { t, i18n } = useTranslation("index");
+
+    useEffect(() => {
+        console.log("THEME MEDIA QUERY CHANGE DETECTED")
+
+        setModeType(isUserModeTypeDark ? ModeType.DARK : ModeType.LIGHT)
+    }, [isUserModeTypeDark])
+
+    useEffect(() => {
+        console.log("THEME CHANGE DETECTED")
+
+        changeWebIconTheme(modeType)
+    }, [modeType])
+
+    useEffect(() => {
+        i18n.changeLanguage(languageType, () => {
+            document.title = t("websiteTitle")
+        })
+    }, [languageType])
+
+    console.log("modeType: " + modeType)
+
+    function changeLanguageType(languageType : LanguageType){
+        setLanguageType(languageType)
+    }
+
+    function changeModeType(modeType : ModeType){
+        setModeType(modeType)
+    }
+
+    return (<StyledEngineProvider injectFirst = { true }>
+        <ThemeProvider theme = {
+            useMemo(() => createAppTheme(modeType, languageType), [modeType, languageType])
+        }>
+                <CssBaseline />
+                <AppTopBar
+                    onChangeLanguageType = { changeLanguageType }
+                    onChangeModeType = { changeModeType }
+                />
+                <AppContainer maxWidth = { false } disableGutters>
+                    <IndexPageFrontContent />
+                    <CustomizedTimeline
+                </AppContainer>
+        </ThemeProvider>
+    </StyledEngineProvider>)
+}
+
+
+
+
+window.onload = main
