8ba1c7255d9093059697c8125451a0b20949191f
DaltonWritingCenter <81346625+DaltonWritingCenter@users.noreply.github.com>
2021-07-10 20:16:18 +0800

    timeline grid fix

--- src/components/IndexPageTimeline.tsx
+++ src/components/IndexPageTimeline.tsx
@@ -139,7 +139,7 @@ const headmasters: headmasterItem[] = [{
     name: "WangZheng",
     position : "XiaoZhang",
     positiontwo : '',
-    year : '2009-',
+    year : '2009-现今',
     picture: headmasterTen,
 }]
 
@@ -194,7 +194,7 @@ function DesktopTimeline() {
                                         height : theme.spacing(7), width : theme.spacing(7)
                                     }} />
                             </Grid>
-                            <Grid item padding = {1}>
+                            <Grid item padding = {1} sx = {{width:160}}>
                                     <Typography variant = 'h5'>{ t(name) }</Typography>
                                 <Typography align = {index % 2 === 0 ? "left" : "right"} variant = 'subtitle1' color = 'textSecondary'>{ t(year) }</Typography>
                                 <Typography align = {index % 2 === 0 ? "left" : "right"} variant = 'subtitle1' color = 'textSecondary'>{ t(position) }</Typography>
